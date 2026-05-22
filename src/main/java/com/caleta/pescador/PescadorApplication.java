00c9b906148205e1e3f7d9d1d10642d21a39827b
BenjitaDEV <benjamin.cespedes2708@gmail.com>
2026-05-22 17:35:52 -0400

    hola

---
+++ src/main/java/com/caleta/pescador/PescadorApplication.java
@@ -0,0 +1,13 @@
+package com.caleta.pescador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PescadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PescadorApplication.class, args);
+	}
+
+}
